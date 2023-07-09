2c78198daebba33991a7de5c86016d3d5a1b6352
Ardian <ardianmaliqaj49@gmail.com>
2023-07-09 13:23:59 +0200

    printing simple format

--- token.ts
+++ token.ts
@@ -4,14 +4,14 @@ import { Primitive } from "./primitive.ts";
 export class Token {
   public token = format(this.constructor.name);
 
-  prettyPrint(indentation = "") {
-    if (this instanceof Primitive) return `${this.value}`;
+  prettyPrint(indentation = "    ") {
+    if (this instanceof Primitive) return `[${this.value}]`;
 
     const properties = Object.entries(this)
       .filter(([_k, v]) => v instanceof Token)
-      .map(([k, v]) => `${indentation}  ${k}: ${v.prettyPrint(`${indentation}|  `)}`)
+      .map(([k, v]) => `${indentation}    ${k} ${v.prettyPrint(`${indentation} | `)}`)
       .join("\n");
 
-    return `\n${properties}\n${indentation}`;
+    return `\n${properties}\n${indentation}\n`;
   }
 }
