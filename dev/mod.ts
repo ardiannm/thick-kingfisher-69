349fbe84edf82508fc12cd849e80512bf9b2294d
Ardian <ardianmaliqaj49@gmail.com>
2023-08-01 12:03:42 +0200

    delete stringify

--- dev/mod.ts
+++ dev/mod.ts
@@ -1,14 +1,13 @@
 // import { Parser, Interpreter } from "https://deno.land/x/amparser@v0.04/mod.ts";
 
 import Parser from "../parser.ts";
-import Stringify from "../stringify.ts";
 
 while (true) {
   console.log();
   const input = prompt(">>") || "";
   const parser = new Parser(input);
   const tree = parser.parse();
-  console.log(Stringify(tree));
+  console.log(tree);
   if (parser.errors.length) {
     console.log();
     console.log(parser.errors.map((e) => e.message));
--- stringify.ts
+++
@@ -1,4 +0,0 @@
-import { Token } from "./token.ts";
-
-const Stringify = (token: Token) => JSON.stringify(token, null, 3);
-export default Stringify;
