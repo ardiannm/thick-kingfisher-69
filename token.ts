08e06b3a3f0b5d9ab7fb9fa8c2f884f266f8e135
Ardian <ardianmaliqaj49@gmail.com>
2023-08-02 18:41:57 +0200

    keep the lower case type names in the tokens

--- token.ts
+++ token.ts
@@ -1,7 +1,3 @@
 export default class Token {
-  public type = this.constructor.name
-    .replace(/[A-Z]/g, (match) => ` ${match}`)
-    .trim()
-    .replace(/[ ]/g, "-")
-    .toLowerCase();
+  public type = this.constructor.name.replace(/[A-Z]/g, (match) => ` ${match}`).trim();
 }
