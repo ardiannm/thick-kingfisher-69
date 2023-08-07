0cf23f3b3b797bcff3df13f63d2cb0e5198b339a
Ardian <ardianmaliqaj49@gmail.com>
2023-08-07 23:23:08 +0200

    formatting token name

--- token.ts
+++ token.ts
@@ -1,4 +1,4 @@
 export default class Token {
-  public token = this.constructor.name;
+  public token = this.constructor.name.replace(/[A-Z]/g, " $&").trim().replace(/ /, "-").toLowerCase();
   constructor(public from: number, public to: number) {}
 }
