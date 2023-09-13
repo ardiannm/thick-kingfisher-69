6d180194c172b537c6f4d09335171544f03ec8ec
Ardian <ardianmaliqaj49@gmail.com>
2023-09-13 15:25:41 +0200

    successfully loggin parse tree in a formated manner

--- src/ast/tokens/SyntaxToken.ts
+++ src/ast/tokens/SyntaxToken.ts
@@ -3,16 +3,16 @@ import Color, { colorize } from "../../services/Color";
 export default class SyntaxToken {
   public type = this.constructor.name;
 
-  toString(indent: string = colorize(".", Color.DimSkyBlue)) {
+  public toString(indent: string = colorize(".", Color.DimSkyBlue)) {
     let str = "";
     for (const prop of Object.getOwnPropertyNames(this)) {
       if (prop === "type") {
         str += `${indent} ${colorize(this.type, Color.Blue)}`;
         continue;
       }
-      if (this[prop] instanceof SyntaxToken) str += `\n${indent}${this[prop].toString(indent + indent)}`;
+      if (this[prop] instanceof SyntaxToken) str += `\n${indent}${this[prop].toString(indent + colorize(".", Color.DimSkyBlue))}`;
       else if (Array.isArray(this[prop])) this[prop].forEach((p: SyntaxToken) => (str += `\n${indent}${p.toString(indent)}`));
-      else str += ` // ${this[prop]}`;
+      else if (this[prop]) str += ` ${colorize(".", Color.DimSkyBlue)} ${colorize(this[prop], Color.Yellow)}`;
     }
     return str;
   }
