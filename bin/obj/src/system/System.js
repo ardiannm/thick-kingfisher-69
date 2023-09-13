7de44839415f38f4d3c3074d7c2ec9230c37119e
Ardian <ardianmaliqaj49@gmail.com>
2023-09-13 09:43:36 +0200

    changing output build path in tsconfig

--- bin/obj/src/system/System.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class System {
-    type = this.constructor.name;
-}
-exports.default = System;
--- dist/koto/ast/tokens/BadToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Character_1 = __importDefault(require("./Character"));
-class BadToken extends Character_1.default {
-}
-exports.default = BadToken;
