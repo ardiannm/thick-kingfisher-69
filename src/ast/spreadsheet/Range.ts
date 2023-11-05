025565a4b6503b2ff83238f2f7741efffb73876e
Ardian <ardianmaliqaj49@gmail.com>
2023-11-05 02:12:27 +0100

    [fix] adding colon in the range token

--- src/ast/spreadsheet/Range.ts
+++ src/ast/spreadsheet/Range.ts
@@ -3,7 +3,7 @@ import Identifier from "../expressions/Identifier";
 
 export default class Range extends Identifier {
   constructor(public left: Cell, public right: Cell) {
-    const view = left.view + right.view;
+    const view = left.view + ":" + right.view;
     super(view);
   }
 }
