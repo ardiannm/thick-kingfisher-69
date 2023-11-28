8634e3919e1d420d5f1abea0cbb5ebf1e561ca60
Ardian <ardianmaliqaj49@gmail.com>
2023-11-28 16:57:28 +0100

    Organizing Evaluator Code

--- src/Evaluator.ts
+++ src/Evaluator.ts
@@ -7,22 +7,22 @@ import { Environment } from "./Environment";
 export class Evaluator {
   constructor(private Env: Environment, public Report: Diagnostics) {}
 
-  public Evaluate<Structure extends SyntaxNode>(Node: Structure) {
+  Evaluate<Structure extends SyntaxNode>(Node: Structure) {
     switch (Node.Kind) {
+      case SyntaxKind.SyntaxTree:
+        return this.SyntaxTree(Node as Structure & SyntaxTree);
       case SyntaxKind.NumberToken:
         return this.NumberToken(Node as Structure & SyntaxToken);
-      case SyntaxKind.ReferenceExpression:
-        return this.ReferenceExpression(Node as Structure & ReferenceExpression);
-      case SyntaxKind.BinaryExpression:
-        return this.BinaryExpression(Node as Structure & BinaryExpression);
       case SyntaxKind.CellReference:
         return this.CellReference(Node as Structure & CellReference);
+      case SyntaxKind.ReferenceExpression:
+        return this.ReferenceExpression(Node as Structure & ReferenceExpression);
       case SyntaxKind.ParenthesizedExpression:
         return this.ParenthesizedExpression(Node as Structure & ParenthesizedExpression);
       case SyntaxKind.UnaryExpression:
         return this.UnaryExpression(Node as Structure & UnaryExpression);
-      case SyntaxKind.SyntaxTree:
-        return this.SyntaxTree(Node as Structure & SyntaxTree);
+      case SyntaxKind.BinaryExpression:
+        return this.BinaryExpression(Node as Structure & BinaryExpression);
       default:
         this.Report.MethodNotImplemented(Node.Kind);
     }
@@ -30,6 +30,36 @@ export class Evaluator {
 
   private SyntaxTree(Node: SyntaxTree) {
     return this.Evaluate(Node.Root);
+  }
+
+  private NumberToken(Node: SyntaxToken): number {
+    return parseFloat(Node.Text);
+  }
+
+  private CellReference(Node: CellReference) {
+    return this.Env.GetValue(Node.Reference);
+  }
+
+  private ReferenceExpression(Node: ReferenceExpression) {
+    const Value = this.Evaluate(Node.Expression);
+    this.Env.SetValue(Node, Value).forEach((r) => this.Evaluate(r));
+    return Value;
+  }
+
+  private ParenthesizedExpression(Node: ParenthesizedExpression) {
+    return this.Evaluate(Node.Expression);
+  }
+
+  private UnaryExpression(Node: UnaryExpression) {
+    const Right = this.Evaluate(Node.Right);
+    switch (Node.Operator.Kind) {
+      case SyntaxKind.PlusToken:
+        return Right;
+      case SyntaxKind.MinusToken:
+        return -Right;
+      default:
+        this.Report.NotAnOperator(Node.Operator.Kind);
+    }
   }
 
   private BinaryExpression(Node: BinaryExpression) {
@@ -47,35 +77,5 @@ export class Evaluator {
       default:
         this.Report.NotAnOperator(Node.Operator.Kind);
     }
-  }
-
-  private UnaryExpression(Node: UnaryExpression) {
-    const Right = this.Evaluate(Node.Right);
-    switch (Node.Operator.Kind) {
-      case SyntaxKind.PlusToken:
-        return Right;
-      case SyntaxKind.MinusToken:
-        return -Right;
-      default:
-        this.Report.NotAnOperator(Node.Operator.Kind);
-    }
-  }
-
-  private ParenthesizedExpression(Node: ParenthesizedExpression) {
-    return this.Evaluate(Node.Expression);
-  }
-
-  private NumberToken(Node: SyntaxToken): number {
-    return parseFloat(Node.Text);
-  }
-
-  private ReferenceExpression(Node: ReferenceExpression) {
-    const Value = this.Evaluate(Node.Expression);
-    this.Env.SetValue(Node, Value).forEach((r) => this.Evaluate(r));
-    return Value;
-  }
-
-  private CellReference(Node: CellReference) {
-    return this.Env.GetValue(Node.Reference);
   }
 }
