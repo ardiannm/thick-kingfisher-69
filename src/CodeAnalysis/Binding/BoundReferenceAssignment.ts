169dec02ef9bfd4d7ffdc5295ea4da604acaa512
Ardian <ardianmaliqaj49@gmail.com>
2023-12-02 22:49:44 +0100

    Adding Subscribe Method To BoundReferenceAssignment For Easier API

--- src/CodeAnalysis/Binding/BoundReferenceAssignment.ts
+++ src/CodeAnalysis/Binding/BoundReferenceAssignment.ts
@@ -6,4 +6,9 @@ export class BoundReferenceAssignment extends BoundWithReference {
   constructor(public Kind: BoundKind, public Reference: string, public Referencing: Array<string>, public ReferencedBy: Array<string>, public Expression: BoundExpression) {
     super(Kind, Reference);
   }
+
+  Subscribe(Reference: string) {
+    if (this.ReferencedBy.includes(Reference)) return;
+    this.ReferencedBy.push(Reference);
+  }
 }
