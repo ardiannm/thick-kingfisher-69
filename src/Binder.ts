d7aef77a63f5bc94eca4ea6464efedd0829f73be
Ardian <ardianmaliqaj49@gmail.com>
2023-12-02 08:42:24 +0100

    Successfully Binding RangeReference

--- src/Binder.ts
+++ src/Binder.ts
@@ -8,12 +8,14 @@ export class Binder {
 
   private DeclarationsStack = new Set<string>();
 
-  Bind<Structure extends SyntaxNode>(Node: Structure) {
+  Bind<Structure extends SyntaxNode>(Node: Structure): BoundNode {
     switch (Node.Kind) {
       case SyntaxKind.SyntaxTree:
         return this.BindSyntaxTree(Node as Structure & SyntaxTree);
+      case SyntaxKind.IdentifierToken:
+        return this.BindIdentifier(Node as Structure & SyntaxToken);
       case SyntaxKind.NumberToken:
-        return this.BindNumberExpression(Node as Structure & SyntaxToken);
+        return this.BindNumber(Node as Structure & SyntaxToken);
       case SyntaxKind.CellReference:
         return this.BindCellReference(Node as Structure & CellReference);
       case SyntaxKind.RangeReference:
@@ -24,53 +26,78 @@ export class Binder {
   }
 
   private BindRangeReference(Node: RangeReference) {
-    const Reference = this.Bind(Node.Left);
-    this.DeclarationsStack.add(Reference);
-    return new BoundCellReference(Binding.BoundCellReference, Reference);
+    const BoundLeft = this.Bind(Node.Left) as BoundLiteral;
+    const BoundRight = this.Bind(Node.Right) as BoundLiteral;
+    return new BoundRangeReference(BindKind.BoundRangeReference, BoundLeft.Reference + ":" + BoundRight.Reference);
   }
 
   private BindCellReference(Node: CellReference) {
     const Reference = Node.Left.Text + Node.Right.Text;
     this.DeclarationsStack.add(Reference);
-    return new BoundCellReference(Binding.BoundCellReference, Reference);
+    return new BoundCellReference(BindKind.BoundCellReference, Reference);
   }
 
-  private BindNumberExpression(Node: SyntaxToken) {
+  private BindIdentifier(Node: SyntaxToken) {
     const Value = parseFloat(Node.Text);
-    return new BoundNumberExpression(Binding.BoundNumberExpression, Value);
+    return new BoundIdentifier(BindKind.BoundNumber, Node.Text, Value);
+  }
+
+  private BindNumber(Node: SyntaxToken) {
+    const Value = parseFloat(Node.Text);
+    return new BoundNumber(BindKind.BoundNumber, Node.Text, Value);
   }
 
   private BindSyntaxTree(Node: SyntaxTree) {
-    return new BoundSyntaxTree(Binding.BoundSyntaxTree, this.Bind(Node.Root));
+    return new BoundSyntaxTree(BindKind.BoundSyntaxTree, this.Bind(Node.Root));
   }
 }
 
-enum Binding {
+enum BindKind {
   BoundSyntaxTree = "BoundSyntaxTree",
-  BoundNumberExpression = "BoundNumberExpression",
+  BoundRangeReference = "BoundRangeReference",
   BoundCellReference = "BoundCellReference",
+  BoundIdentifier = "BoundIdentifier",
+  BoundNumber = "BoundNumber",
 }
 
 class BoundNode {
-  constructor(public Kind: Binding) {}
+  constructor(public Kind: BindKind) {}
 }
 
 class BoundSyntaxTree extends BoundNode {
-  constructor(public Kind: Binding, public Root: BoundExpression) {
+  constructor(public Kind: BindKind, public Root: BoundExpression) {
     super(Kind);
   }
 }
 
 class BoundExpression extends BoundNode {}
 
-class BoundNumberExpression extends BoundExpression {
-  constructor(public Kind: Binding, public Value: number) {
+class BoundLiteral extends BoundExpression {
+  constructor(public Kind: BindKind, public Reference: string) {
     super(Kind);
   }
 }
 
-class BoundCellReference extends BoundExpression {
-  constructor(public Kind: Binding, public Reference: string) {
+class BoundNumber extends BoundLiteral {
+  constructor(public Kind: BindKind, public Reference: string, public Value: number) {
+    super(Kind, Reference);
+  }
+}
+
+class BoundIdentifier extends BoundLiteral {
+  constructor(public Kind: BindKind, public Reference: string, public Value: number) {
+    super(Kind, Reference);
+  }
+}
+
+class BoundCellReference extends BoundLiteral {
+  constructor(public Kind: BindKind, public Reference: string) {
+    super(Kind, Reference);
+  }
+}
+
+class BoundRangeReference extends BoundExpression {
+  constructor(public Kind: BindKind, public Reference: string) {
     super(Kind);
   }
 }
