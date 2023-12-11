96fa13d3a30f79e8ab22f9dc7e888fcbe4a513c8
Ardian <ardianmaliqaj49@gmail.com>
2023-12-11 01:35:31 +0100

    Adding Descriptive Comments In Interpreter

--- src/IO/Interpreter.ts
+++ src/IO/Interpreter.ts
@@ -14,23 +14,27 @@ export class Interpreter {
   private Width = 0;
 
   constructor() {
-    this.GenerateStart();
+    this.InitializeBuffer();
   }
 
+  // Main method to run the interpreter loop
   Run() {
     while (true) {
       const InputLine = Promp.question("> ");
       console.clear();
+
       // Provide a way to exit the loop
       if (InputLine.toLowerCase() === "exit") {
         break;
       }
-      // Provide a way to exit the loop
+
+      // Provide a way to reset the interpreter state
       if (InputLine.toLowerCase() === "reset") {
-        this.GenerateStart();
+        this.InitializeBuffer();
         continue;
       }
-      // Clear the last line
+
+      // Clear the last line in the buffer and console
       if (InputLine.toLowerCase() === "cls") {
         console.clear();
         this.Buffer.pop();
@@ -38,45 +42,54 @@ export class Interpreter {
         this.Report(this.Input(), Message);
         continue;
       }
+
       if (InputLine.trim()) {
         // Concatenate the input to the buffer
         this.Buffer.push(InputLine);
       }
+
       try {
+        // Parse and evaluate the input syntax tree
         const Tree = SyntaxTree.Bind(this.Input(), this.Environment);
         const Evaluation = new Evaluator(this.Environment).Evaluate(Tree);
         const Value = JSON.stringify(Evaluation);
         this.Report(this.Input(), Value);
       } catch (error) {
+        // Report any errors that occur during parsing or evaluation
         this.Report(this.Input(), (error as Diagnostic).Message);
       }
     }
   }
 
-  private GenerateStart() {
+  // Initialize the interpreter by loading source text from a file
+  private InitializeBuffer() {
     console.clear();
-    this.Buffer = this.Report(this.LoadSource(), "Interpreter: Source text content from file.").split("\n");
+    this.Buffer = this.Report(this.LoadSource()).split("\n");
     for (const Line of this.Buffer) {
       this.Width = Math.max(this.Width, Line.length);
     }
   }
 
+  // Load source text content from a file
   private LoadSource(): string {
     const FullPath = path.join(".", "src", "IO", ".lang");
     return fs.readFileSync(FullPath, "utf8");
   }
 
+  // Display a formatted message in the console
   private Report(Str: string = "", Message?: string) {
     console.log();
     console.log(Str);
     if (Message) {
-      console.log("-".repeat(Math.max(Message.length, this.Width)));
+      // const Seperator = "-".repeat(Math.max(Message.length, this.Width));
+      console.log(/* Seperator */);
       console.log(Message);
     }
     console.log();
     return Str;
   }
 
+  // Combine the lines in the buffer into a single input string
   private Input() {
     return this.Buffer.join("\n");
   }
