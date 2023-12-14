65e078d3676cde595dce439a1ff30644fd528016
Ardian <ardianmaliqaj49@gmail.com>
2023-12-14 02:35:17 +0100

    Massively Simplifying Lexer

--- src/CodeAnalysis/Lexer.ts
+++ src/CodeAnalysis/Lexer.ts
@@ -9,6 +9,7 @@ export class Lexer {
   private Index = 0;
   private Diagnostics = new DiagnosticBag(DiagnosticKind.Lexer);
   private Start = this.Index;
+  private Kind = SyntaxKind.EndOfFileToken;
 
   constructor(public readonly Source: SourceText) {}
 
@@ -49,37 +50,53 @@ export class Lexer {
 
   Lex(): SyntaxToken {
     this.Start = this.Index;
+    this.Kind = Facts.Kind(this.Char);
 
-    if (this.IsLetter(this.Char)) {
-      return this.ParseIdentifier();
-    }
-    if (this.IsDigit(this.Char)) {
-      return this.ParseNumberToken();
-    }
-    if (this.IsSpace(this.Char)) {
-      return this.ParseSpaceToken();
-    }
-    if (this.Match(SyntaxKind.HashToken)) {
-      return this.ParseCommentToken();
-    }
-    if (this.Match(SyntaxKind.MinusToken, SyntaxKind.GreaterToken)) {
-      this.Index += 2;
-      return new SyntaxToken(SyntaxKind.PointerToken, this.Text);
-    }
-    if (this.Match(SyntaxKind.BadToken)) {
-      throw this.Diagnostics.BadTokenFound(this.Char);
+    switch (this.Kind) {
+      case SyntaxKind.BadToken:
+        // If bad token found it may either be a letter or number or space token
+
+        if (this.IsLetter(this.Char)) {
+          return this.ParseIdentifier();
+        }
+        if (this.IsDigit(this.Char)) {
+          return this.ParseNumberToken();
+        }
+        if (this.IsSpace(this.Char)) {
+          return this.ParseSpaceToken();
+        }
+
+        // if all else fails then throw a bad token error
+        throw this.Diagnostics.BadTokenFound(this.Char);
+
+      case SyntaxKind.HashToken:
+        // Special case for hash token when it comes to comments
+        return this.ParseCommentToken();
+
+      case SyntaxKind.MinusToken:
+        // special case for minus token
+        if (this.Match(SyntaxKind.MinusToken, SyntaxKind.GreaterToken)) {
+          // increment one more to account for the next token
+          this.Index += 1;
+          this.Kind = SyntaxKind.PointerToken;
+        }
     }
 
+    // else by default increment index position by one for all cases
     this.Index += 1;
 
-    return new SyntaxToken(Facts.Kind(this.Text), this.Text);
+    return new SyntaxToken(this.Kind, this.Text);
   }
 
+  // parse identifier tokens
   private ParseIdentifier() {
-    while (this.IsLetter(this.Char)) this.Index += 1;
+    while (this.IsLetter(this.Char)) {
+      this.Index += 1;
+    }
     return new SyntaxToken(Facts.KeywordTokenKind(this.Text), this.Text);
   }
 
+  // parse comment tokens
   private ParseCommentToken() {
     while (true) {
       this.Index += 1;
@@ -89,20 +106,26 @@ export class Lexer {
     return new SyntaxToken(SyntaxKind.CommentToken, this.Text);
   }
 
+  // parse space tokens
   private ParseSpaceToken() {
-    while (this.IsSpace(this.Char)) this.Index += 1;
+    while (this.IsSpace(this.Char)) {
+      this.Index += 1;
+    }
     return new SyntaxToken(SyntaxKind.SpaceToken, this.Text);
   }
 
+  // parse number tokens
   private ParseNumberToken() {
-    while (this.IsDigit(this.Char)) this.Index += 1;
+    while (this.IsDigit(this.Char)) {
+      this.Index += 1;
+    }
     if (this.Match(SyntaxKind.DotToken)) {
       this.Index += 1;
-      if (!this.IsDigit(this.Char)) {
-        throw this.Diagnostics.WrongFloatingNumberFormat();
-      }
+      if (!this.IsDigit(this.Char)) throw this.Diagnostics.WrongFloatingNumberFormat();
     }
-    while (this.IsDigit(this.Char)) this.Index += 1;
+    while (this.IsDigit(this.Char)) {
+      this.Index += 1;
+    }
     return new SyntaxToken(SyntaxKind.NumberToken, this.Text);
   }
 }
