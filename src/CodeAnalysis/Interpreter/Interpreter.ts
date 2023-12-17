a0398867c9770ad2fca37fbc833ca20dbc0ca7f3
Ardian <ardianmaliqaj49@gmail.com>
2023-12-17 03:07:56 +0100

    redefining methods within writer, previous code badly written

--- src/CodeAnalysis/Interpreter/Interpreter.ts
+++ src/CodeAnalysis/Interpreter/Interpreter.ts
@@ -53,12 +53,13 @@ export class Interpreter {
       try {
         console.clear();
         const Tree = SyntaxTree.Parse(InputLine);
-        // console.log(JSON.stringify(Tree, undefined, 2));
         const Written = new Rewriter().Rewrite(Tree);
-        console.log();
-        console.log(Interpreter.Color(DiagnosticKind.Rewriter, Color.Teal));
-        console.log();
-        console.log(JSON.stringify(Written.ObjectId, undefined, 2));
+
+        console.log(Interpreter.Color("------------------------ TREE -------------------------------------", Color.Teal));
+        console.log(JSON.stringify(Tree, undefined, 2));
+        console.log(Interpreter.Color("----------------------- REWRITER ----------------------------------", Color.Teal));
+        console.log(JSON.stringify(Written, undefined, 2));
+
         // this.Evaluate();
       } catch (error) {
         this.ErrorHandler(error as Error);
--- src/CodeAnalysis/Rewriter/Rewriter.ts
+++ src/CodeAnalysis/Rewriter/Rewriter.ts
@@ -1,8 +1,6 @@
 import { DiagnosticBag } from "../Diagnostics/DiagnosticBag";
 import { DiagnosticKind } from "../Diagnostics/DiagnosticKind";
 import { BinaryExpression } from "../Parser/BinaryExpression";
-import { Facts } from "../Parser/Facts";
-import { ParenthesizedExpression } from "../Parser/ParenthesizedExpression";
 import { Program } from "../Parser/Program";
 import { SyntaxKind } from "../Parser/SyntaxKind";
 import { SyntaxNode } from "../Parser/SyntaxNode";
@@ -23,55 +21,20 @@ export class Rewriter {
         return this.RewriteProgram(Node as NodeType<Program>);
       case SyntaxKind.UnaryExpression:
         return this.RewriteUnaryExpression(Node as NodeType<UnaryExpression>);
-      case SyntaxKind.ParenthesizedExpression:
-        return this.RewriteParenthesizedExpression(Node as NodeType<ParenthesizedExpression>);
-      case SyntaxKind.BinaryExpression:
-        return this.RewriteBinaryExpression(Node as NodeType<BinaryExpression>);
       default:
         throw this.Diagnostics.MissingMethod(Node.Kind);
     }
   }
 
   private RewriteProgram(Node: Program) {
-    Node.Root = Node.Root.map((Statement) => this.Rewrite(Statement));
-    return Node;
+    const Root = Node.Root.map((Statement) => this.Rewrite(Statement));
+    return new Program(SyntaxKind.Program, Root);
   }
 
-  private RewriteUnaryExpression(Node: UnaryExpression) {
-    const Right = this.Rewrite(Node.Right) as UnaryExpression;
+  private RewriteUnaryExpression(Node: UnaryExpression): SyntaxNode {
+    const Right = Node.Right as UnaryExpression;
     if (Right.Kind === SyntaxKind.UnaryExpression && Right.Operator.Kind === Node.Operator.Kind) {
-      return this.Rewrite(Right.Right);
-    }
-    Node.Right = this.Rewrite(Node.Right);
-    if (Node.Operator.Kind === SyntaxKind.PlusToken) return this.Rewrite(Node.Right);
-    return Node;
-  }
-
-  private RewriteParenthesizedExpression(Node: ParenthesizedExpression) {
-    return this.Rewrite(Node.Expression);
-  }
-
-  private RewriteBinaryExpression(Node: BinaryExpression) {
-    Node.Left = this.Rewrite(Node.Left);
-
-    if (Node.Left.Kind === SyntaxKind.UnaryExpression) {
-      Node.Left = this.SwitchOperator(Node.Left);
-    }
-
-    if (Node.Operator.Kind === SyntaxKind.MinusToken && Node.Right.Kind === SyntaxKind.ParenthesizedExpression) {
-      Node.Right = this.SwitchOperator(Node.Right);
-    }
-
-    Node.Right = this.Rewrite(Node.Right);
-
-    var Right = Node.Right as BinaryExpression;
-    if (Right.Kind === SyntaxKind.BinaryExpression) {
-      const Precedence = Facts.BinaryPrecedence(Right.Operator.Kind) === Facts.BinaryPrecedence(Node.Operator.Kind);
-      if (Precedence) {
-        const Left = new BinaryExpression(SyntaxKind.BinaryExpression, Node.Left, Node.Operator, this.Rewrite(Right.Left));
-        const Written = new BinaryExpression(SyntaxKind.BinaryExpression, Left, Right.Operator, this.Rewrite(Right.Right));
-        return this.Rewrite(Written);
-      }
+      return this.RewriteUnaryExpression(Right) as SyntaxNode;
     }
     return Node;
   }
@@ -85,10 +48,6 @@ export class Rewriter {
         return Node;
       case SyntaxKind.BinaryExpression:
         return this.SwitchBinaryExpression(Node as NodeType<BinaryExpression>);
-      case SyntaxKind.ParenthesizedExpression:
-        return this.SwitchOperator((Node as NodeType<ParenthesizedExpression>).Expression);
-      case SyntaxKind.UnaryExpression:
-        return this.SwitchOperator((Node as NodeType<UnaryExpression>).Right);
       default:
         throw this.Diagnostics.MissingSwitchMethod(Node.Kind);
     }
@@ -97,12 +56,10 @@ export class Rewriter {
   private SwitchBinaryExpression(Node: BinaryExpression) {
     switch (Node.Operator.Kind) {
       case SyntaxKind.PlusToken:
-        return new BinaryExpression(SyntaxKind.BinaryExpression, this.SwitchOperator(Node.Left), new SyntaxToken(SyntaxKind.MinusToken, "-"), this.SwitchOperator(Node.Right));
+        return new BinaryExpression(SyntaxKind.BinaryExpression, Node.Left, new SyntaxToken(SyntaxKind.MinusToken, "-"), Node.Right);
       case SyntaxKind.MinusToken:
-        return new BinaryExpression(SyntaxKind.BinaryExpression, this.SwitchOperator(Node.Left), new SyntaxToken(SyntaxKind.PlusToken, "+"), this.SwitchOperator(Node.Right));
+        return new BinaryExpression(SyntaxKind.BinaryExpression, Node.Left, new SyntaxToken(SyntaxKind.PlusToken, "+"), Node.Right);
     }
-    Node.Left = this.Rewrite(Node.Left);
-    Node.Right = this.Rewrite(Node.Right);
     return Node;
   }
 }
