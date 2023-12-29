696b8e498a07a5690e7df3005f79e0bf196a7fd7
Ardian <ardianmaliqaj49@gmail.com>
2023-12-29 19:49:14 +0100

    debug(subjects): extracting subjects properly

--- src/BoundScope.ts
+++ src/BoundScope.ts
@@ -59,12 +59,28 @@ export class BoundScope {
       const Data = new Cell(Node.Name, 0, Node.Expression, Node.Subjects, new Set<string>());
       this.Data.set(Node.Name, Data);
       for (const Subject of Data.Subjects) this.GetCell(Subject)?.Notify(Node.Name);
+      this.GetSubjects(Data.Name, Data);
       return Data;
     }
     for (const Subject of Data.Subjects) this.GetCell(Subject)?.DoNotNotify(Node.Name);
     Data.Subjects = Node.Subjects;
     for (const Subject of Data.Subjects) this.GetCell(Subject)?.Notify(Node.Name);
     Data.Subjects = Node.Subjects;
+    this.GetSubjects(Data.Name, Data);
     return Data;
+  }
+
+  private GetSubjects(Name: string, Node: Cell, Subjects = new Set<string>()) {
+    if (Node.Subjects.has(Name)) {
+      this.Diagnostics.ReportCircularDependency(DiagnosticPhase.Binder, Name, Node.Name);
+      return Subjects;
+    }
+    for (const Subject of Node.Subjects) {
+      Subjects.add(Subject);
+      const SubjectNode = this.GetCell(Subject);
+      if (SubjectNode === undefined) return undefined;
+      this.GetSubjects(Name, SubjectNode, Subjects);
+    }
+    return Subjects;
   }
 }
