8b5696956b4d3e14887cf88129add92204ba656d
Ardian <ardianmaliqaj49@gmail.com>
2024-04-17 22:31:26 +0200

    dev: show the final output value when no severe real errors found
    - frontend ui for runtime testing using solidjs
    - minor improvements and bug fixes

--- client/src/components/screen.tsx
+++ client/src/components/screen.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/screen.module.scss";
 import { SyntaxTree } from "../../../src/analysis/parser/syntax.tree";
 import { Diagnostic } from "../../../src/analysis/diagnostics/diagnostic";
 import { createEffect, createSignal, For, Show, type Component } from "solid-js";
+import { DiagnosticSeverity } from "../../../src/analysis/diagnostics/diagnostic.severity";
 
 type Input = InputEvent & {
   currentTarget: HTMLTextAreaElement;
@@ -29,10 +30,12 @@ const Input: Component = () => {
 
   const handleTextAreaInput = (e: Input) => setText(e.target.value);
 
+  const severeErrors = (a: Diagnostic) => a.Severity === DiagnosticSeverity.Error;
+
   return (
     <div class={styles.input}>
-      <textarea class={styles.textArea} spellcheck={false} oninput={handleTextAreaInput} value={text()}></textarea>
-      <Show when={diagnostics().length > 0} fallback={<div class={styles.value}>{value()}</div>}>
+      <textarea class={styles.textArea} spellcheck={false} oninput={handleTextAreaInput} value={text()} autofocus={true}></textarea>
+      <Show when={diagnostics().find(severeErrors)} fallback={<div class={styles.value}>{value()}</div>}>
         <div class={styles.diagnostics}>
           <For each={diagnostics()}>
             {(diagnostic) => {
