c82c3859e84993540324cf6dd31f80d220255594
Ardian <ardianmaliqaj49@gmail.com>
2024-07-28 10:12:38 +0200

    chore: cleaning up code

--- src/analysis/diagnostics/diagnostic.ts
+++ src/analysis/diagnostics/diagnostic.ts
@@ -3,13 +3,9 @@ import { Span } from "../text/span";
 import { Severity } from "./severity";
 
 export class Diagnostic {
-  private constructor(private text: SourceText, public severity: Severity, public message: string, public span: Span) {}
+  private constructor(public text: SourceText, public severity: Severity, public message: string, public span: Span) {}
 
-  public static createFrom(text: SourceText, message: string, severity: Severity, location: Span) {
-    return new Diagnostic(text, severity, message, location);
-  }
-
-  public get line() {
-    return this.text.getLineSpan(this.span.start);
+  public static createFrom(text: SourceText, message: string, severity: Severity, span: Span) {
+    return new Diagnostic(text, severity, message, span);
   }
 }
