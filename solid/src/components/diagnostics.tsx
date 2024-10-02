954330316c8e63186f13882b2a22ccfe856fa6cb
Ardian <ardianmaliqaj49@gmail.com>
2024-10-02 14:43:11 +0200

    debug: showing diagnostics only when there is any

--- solid/src/components/diagnostics.tsx
+++ solid/src/components/diagnostics.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/editor.module.scss";
-import { type Component, For, Accessor } from "solid-js";
+import { type Component, For, Accessor, Show } from "solid-js";
 import { Diagnostic } from "../../../src/analysis/diagnostics/diagnostic";
 
 export interface DiagnosticsProps {
@@ -8,17 +8,19 @@ export interface DiagnosticsProps {
 
 export const Diagnostics: Component<DiagnosticsProps> = (props: DiagnosticsProps) => {
   return (
-    <div class={styles.diagnostics}>
-      <For each={props.bag()}>
-        {(diagnostic) => (
-          <div class={styles.diagnostic}>
-            <div class={styles.position}>
-              {diagnostic.span.line}:{diagnostic.span.offset}
-            </div>
-            <div class={styles.message}>{diagnostic.message}</div>
-          </div> // Single JSX element
-        )}
-      </For>
-    </div>
+    <Show when={props.bag().length}>
+      <div class={styles.diagnostics}>
+        <For each={props.bag()}>
+          {(diagnostic) => (
+            <div class={styles.diagnostic}>
+              <div class={styles.position}>
+                {diagnostic.span.line}:{diagnostic.span.offset}
+              </div>
+              <div class={styles.message}>{diagnostic.message}</div>
+            </div> // Single JSX element
+          )}
+        </For>
+      </div>
+    </Show>
   );
 };
