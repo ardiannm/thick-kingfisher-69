d16c2ee5e00abd8abdea88e0374b0c882231dc80
Ardian <ardianmaliqaj49@gmail.com>
2024-10-24 23:34:13 +0200

    deleting sidebar, adding basic styles and routing pages

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { ViewportComponent } from './viewport/viewport.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent, ViewportComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ViewportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'editor',
     component: EditorComponent,
-    data: { codeInput: 'Initial code value' },
   },
 ];
