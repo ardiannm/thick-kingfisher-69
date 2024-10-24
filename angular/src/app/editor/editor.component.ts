9550660339a11a1de322571018ef39dbdd3c6d16
Ardian <ardianmaliqaj49@gmail.com>
2024-10-24 21:47:32 +0200

    dev: adding editor component lines for tokenization

--- angular/src/app/editor/editor.component.ts
+++ angular/src/app/editor/editor.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal, computed } from '@angular/core';
+
+const txt = `import {Component} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: \`
+    Hello world!
+  \`,
+})
+export class PlaygroundComponent {}
+
+bootstrapApplication(PlaygroundComponent);
+`;
 
 @Component({
   selector: 'app-editor',
   standalone: true,
   imports: [],
   templateUrl: './editor.component.html',
-  styleUrl: './editor.component.scss'
+  styleUrl: './editor.component.scss',
 })
 export class EditorComponent {
+  @Input('codeInput') sourceText = txt;
 
+  code = signal(txt);
+
+  rows = computed(() => this.code().split('\n'));
 }
