b72ea0f3339c6ec51aaa304d1e9150c5843836b8
Ardian <ardianmaliqaj49@gmail.com>
2024-10-24 20:32:15 +0200

    adding viewport

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ViewportComponent } from './viewport/viewport.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent, ViewportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
