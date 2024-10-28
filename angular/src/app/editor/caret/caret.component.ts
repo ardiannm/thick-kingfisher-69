108423a6e8876474b2511c98b1a369ec9f931a93
Ardian <ardianmaliqaj49@gmail.com>
2024-10-28 20:28:33 +0100

    caret: stoping blinking animation when caret moves position on input

--- angular/src/app/editor/caret/caret.component.ts
+++ angular/src/app/editor/caret/caret.component.ts
@@ -1,15 +1,24 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-caret',
   standalone: true,
   imports: [],
   templateUrl: './caret.component.html',
-  styleUrl: './caret.component.scss',
+  styleUrls: ['./caret.component.scss'],
 })
-export class CaretComponent {
-  @HostBinding('style.left.px') @Input() column = 0;
-  @HostBinding('style.top.px') @Input() line = 0;
-  @HostBinding('style.width.px') @Input() width = 4;
+export class CaretComponent implements OnChanges {
+  @HostBinding('style.left.px') @Input() x = 0;
+  @HostBinding('style.top.px') @Input() y = 0;
+  @HostBinding('style.width.px') @Input() width = 8;
   @HostBinding('style.height.px') @Input() height = 19;
+
+  @HostBinding('class.stop') disableBlink = false;
+
+  ngOnChanges(): void {
+    this.disableBlink = true;
+    if (this.disableBlink) {
+      setTimeout(() => (this.disableBlink = false), 250);
+    }
+  }
 }
