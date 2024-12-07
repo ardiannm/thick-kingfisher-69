17fd0be594e40636f215752596b11154185c0c10
Ardian <ardianmaliqaj49@gmail.com>
2024-12-07 19:47:52 +0100

    moving cursor and formatting the editor component
    - issue with cursor not moving anymore for some reason

--- svelte/src/lib/EditorComponent.svelte
+++ svelte/src/lib/EditorComponent.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
 	import CursorComponent from './CursorComponent.svelte';
-
-	import { onMount } from 'svelte';
 	import { SourceText } from '../../../../parser/ng';
 
-	const text = $state(`@Component({
-})
- 
+	const code = `A1 :: A4
+A5 :: A2
+A2 :: A1+3
+A3 :: A2+5
+A4 :: A3+A2+A5
+A3 :: 1
+`;
 
-export class AppComponent {}`);
-
-	const tree = $derived(SourceText.createFrom(text));
+	const text = $state(code);
+	const tree = $derived(SourceText.parse(text));
 	const lines = $derived(tree.getLines());
+	const diagnostics = $derived(tree.diagnosticsBag.diagnostics);
 
-	let renderCursor = $state(false);
+	let renderCursor = $state(true);
 	let cursor = $state(0);
 	let line = $derived(tree.getLine(cursor));
 	let column = $derived(tree.getColumn(cursor));
 	let x = $state(0);
 	let y = $state(0);
+	// svelte-ignore state_referenced_locally
+	let prevColumn = column;
 
 	function handleKey(event: KeyboardEvent) {
 		const input = event.key;
@@ -26,6 +30,10 @@ export class AppComponent {}`);
 			tranformCaretX();
 		} else if (input === 'ArrowLeft') {
 			tranformCaretX(-1);
+		} else if (input === 'ArrowDown') {
+			tranformCaretY();
+		} else if (input === 'ArrowUp') {
+			tranformCaretY(-1);
 		}
 	}
 
@@ -33,20 +41,27 @@ export class AppComponent {}`);
 		const pos = getPosition(line, column);
 		x = pos.x;
 		y = pos.y;
+		console.log(pos);
 	}
 
 	function tranformCaretX(step = 1) {
 		const newPos = cursor + step;
 		if (newPos >= 0 && newPos <= text.length) {
 			cursor = newPos;
-			updatePosition();
+			prevColumn = column;
 		}
 	}
 
-	$effect(() => {
-		const token = tree.getTokens()[tree.getTokenAt(cursor)];
-		console.log(token.span.text, token.kind);
-	});
+	function tranformCaretY(steps = 1) {
+		const prevLine = line + steps;
+		if (prevLine > 0) {
+			const pos = tree.getPosition(prevLine, prevColumn);
+			cursor = pos;
+		} else {
+			prevColumn = 1;
+			cursor = 0;
+		}
+	}
 
 	interface Position {
 		x: number;
@@ -55,7 +70,9 @@ export class AppComponent {}`);
 	}
 
 	function getPosition(line: number, column: number): Position {
-		const lineElement = document?.getElementById(`line-${line}`)!;
+		const lineElement = document.getElementById(`line-${line}`)!;
+		console.log(lineElement);
+
 		let charCount = 0;
 		column--;
 		for (const child of Array.from(lineElement.childNodes)) {
@@ -98,44 +115,70 @@ export class AppComponent {}`);
 		return { x: rect.x, y: rect.y, height: rect.height };
 	}
 
-	onMount(() => {
-		renderCursor = true;
-		updatePosition();
-	});
+	$effect(() => updatePosition());
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<div>
-	{#each lines as line, index}
-		<div id={`line-${index + 1}`} class="line">
-			{#each line.getTokens() as token}
-				<span class={token.class}>
-					{token.span.text.replace('\n', ' ')}
-				</span>
-			{/each}
-		</div>
-	{/each}
+<div class="editor">
+	<h2>Todo</h2>
+	<ul>
+		<li>update position on window resize</li>
+	</ul>
 
-	{#if renderCursor}
-		<CursorComponent {x} {y} />
-	{/if}
+	<div class="space">
+		{#each lines as line, index}
+			<div id={`line-${index + 1}`} class="line">
+				{#each line.getTokens() as token}
+					<span class={token.class}>
+						{#if token.span.text}
+							{token.span.text}
+						{:else}
+							&nbsp;
+						{/if}
+					</span>
+				{/each}
+			</div>
+		{/each}
+		{#if renderCursor}
+			<CursorComponent {x} {y} />
+		{/if}
+	</div>
+	<div class="stats">
+		{cursor}
+		<br />
+		line {line} column {column}
+	</div>
 
-	{cursor}
-	<br />
-	line {line} column {column}
+	<div class="diagnostics">
+		{#each diagnostics as diagnostic}
+			<span>{diagnostic.message}</span>
+		{/each}
+	</div>
 </div>
 
 <svelte:window on:keydown={handleKey} />
 
 <style scoped>
+	.editor {
+		display: flex;
+		margin: auto;
+		margin-top: auto;
+		flex-direction: column;
+		width: fit-content;
+		height: fit-content;
+		border: 1px solid lightseagreen;
+		padding: 10px;
+		font-family: SuisseIntl-Regular, Helvetica, Arial, sans-serif;
+		font-size: 14px;
+	}
+	.space {
+		width: 700px;
+	}
 	.line {
 		display: flex;
 		flex-direction: row;
 		min-height: 16px;
 		border: 1px solid transparent;
 		box-sizing: border-box;
-		padding-left: 7px;
-		border-bottom: 1px solid red;
 	}
 	span {
 		display: flex;
@@ -149,11 +192,20 @@ export class AppComponent {}`);
 		box-sizing: border-box;
 		white-space: pre;
 	}
-
+	.end-of-file-token,
 	.line-break-trivia,
 	.space-trivia {
 		display: inline-block;
 		width: 1px;
 		height: 1em;
+	}
+	.stats {
+		margin-top: 20px;
+	}
+	.diagnostics {
+		margin-top: 20px;
+	}
+	.space {
+		border: 1px solid lightseagreen;
 	}
 </style>
